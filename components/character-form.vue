1002d73e6a8ffe50b33da5c09ebdf1b10b01d2a8
DaleMatthews <dem7w2@mst.edu>
2018-01-21 01:05:24 -0600

    create character-add card and corresponding dialog

---
+++ components/character-form.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container grid-list-md>
+    <v-layout wrap>
+      <v-flex xs12 sm6 md4>
+        <v-text-field label="Legal first name" required></v-text-field>
+      </v-flex>
+      <v-flex xs12 sm6 md4>
+        <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
+      </v-flex>
+      <v-flex xs12 sm6 md4>
+        <v-text-field label="Legal last name" hint="example of persistent helper text"
+          persistent-hint
+          required
+        ></v-text-field>
+      </v-flex>
+      <v-flex xs12>
+        <v-text-field label="Email" required></v-text-field>
+      </v-flex>
+      <v-flex xs12>
+        <v-text-field label="Password" type="password" required></v-text-field>
+      </v-flex>
+      <v-flex xs12 sm6>
+        <v-select
+          label="Age"
+          required
+          :items="['0-17', '18-29', '30-54', '54+']"
+        ></v-select>
+      </v-flex>
+      <v-flex xs12 sm6>
+        <v-select
+          label="Interests"
+          multiple
+          autocomplete
+          chips
+          :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+        ></v-select>
+      </v-flex>
+    </v-layout>
+    <small>*indicates required field</small>
+  </v-container>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,49 @@
 <template>
-  <v-container grid-list-md>
+  <v-container grid-list-xl>
     <h1 class="display-1">Characters</h1>
     <v-layout row wrap justify-left align-center>
-      <v-flex xs12 sm6 md4 lg2 v-for="c in characters">
-        <v-card router :to="'/character/' + c.name">
-          <v-card-media :src="c.img" height="200px"></v-card-media>
-          <v-card-title class="headline">{{c.name}}</v-card-title>
-          <v-card-text class="subheading">{{c.description}}</v-card-text>
+
+      <v-dialog v-model="showCharacterForm" persistent max-width="500px">
+        <v-card>
+          <v-card-title class="headline">New Character</v-card-title>
+          <v-card-text>
+            <CharacterForm></CharacterForm>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" flat @click.native="showCharacterForm = false">Cancel</v-btn>
+            <v-btn color="blue darken-1" flat @click.native="showCharacterForm = false">Create</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+      <v-flex @click.stop="showCharacterForm = true" xs12 sm6 md4 lg3>
+        <v-card hover class="d-flex" height="330px">
+          <v-layout column justify-center>
+            <v-card-text class="text-xs-center"><v-icon class="xxl">add</v-icon></v-card-text>
+          </v-layout>
         </v-card>
       </v-flex>
+
+      <v-flex xs12 sm6 md4 lg3 v-for="c in characters" :key="c.name">
+        <v-card hover height="330px" router :to="'/character/' + c.name">
+          <v-card-media :src="c.img" height="270px"></v-card-media>
+          <v-card-title class="headline">{{c.name}}</v-card-title>
+        </v-card>
+      </v-flex>
+
     </v-layout>
   </v-container>
 </template>
 
 <script>
   // import axios from 'axios';
+  import CharacterForm from '~/components/character-form';
 
   export default {
+    components: {
+      CharacterForm,
+    },
     // async asyncData() {
     //   let { data } = await axios.get('https://api.magicthegathering.io/v1/cards');
     //   return { cards: data.cards };
@@ -33,6 +60,7 @@
           { img: '/yasha.jpg', name: 'Yasha', description: 'Aasimar Barbarian' },
           { img: '/zink.png', name: 'Zink', description: 'Gnome Warlock' },
         ],
+        showCharacterForm: false,
       };
     },
     created() {
@@ -40,3 +68,9 @@
     },
   };
 </script>
+
+<style>
+.icon.xxl {
+  font-size: 16em;
+}
+</style>
