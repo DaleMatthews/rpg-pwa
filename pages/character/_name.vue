1fa74aa730cecf7c870e2bc88cac86deb91159bd
DaleMatthews <dem7w2@mst.edu>
2018-02-04 03:06:35 -0600

    Fetch spell and format

--- pages/character/_name.vue
+++ pages/character/_name.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container fill-height fluid pa-0>
-    <v-layout col justify-left align-center>
+    <v-layout col justify-left align-top>
       <v-flex d-flex xs3 pl-0 fill-height >
         <v-list>
           <v-list-group v-for="item in categories" :value="item.active" :key="item.title">
@@ -15,20 +15,29 @@
                 <v-icon>keyboard_arrow_down</v-icon>
               </v-list-tile-action>
             </v-list-tile>
-            <v-list-tile v-for="subItem in item.items" v-bind:key="subItem" @click="getAbility">
+            <v-list-tile v-for="subItem in item.items" :key="subItem.name" @click="getAbility(subItem)">
               <v-list-tile-content>
-                <v-list-tile-title>{{ subItem }}</v-list-tile-title>
+                <v-list-tile-title>{{ subItem.name }}</v-list-tile-title>
               </v-list-tile-content>
               <v-list-tile-action>
                 <v-icon>remove</v-icon>
               </v-list-tile-action>
             </v-list-tile>
           </v-list-group>
-          <v-spacer></v-spacer>
         </v-list>
       </v-flex>
-      <v-flex xs9>
-        {{ action && action.name }}
+      <v-flex pa-4 xs9>
+        <template v-if="action">
+          <div class="display-1">{{ action.name }}</div>
+          <v-layout my-4 wrap align-content-start>
+            <v-flex pa-2 xs4 md3 v-for="a in actionData" :key="a.title">
+              <div class="title">{{ a.title }}</div>
+              <div class="body-1">{{ a.value }}</div>
+            </v-flex>
+            <v-flex pt-3 class="subheading" v-for="d in action.desc" :key="$index">{{ d }}</v-flex>
+            <v-flex pt-3 class="subheading"><strong>At higher levels: </strong>{{ action.higher_level.join('\n') }}</v-flex>
+          </v-layout>
+        </template>
       </v-flex>
     </v-layout>
   </v-container>
@@ -41,35 +50,69 @@
   export default {
     data() {
       return {
+        action: null,
         categories: [
           {
             title: 'Standard Actions',
-            items: ['Test1', 'Test2'],
+            items: [
+              {
+                name: 'Asdf',
+                path: 'spells',
+                index: 1,
+              },
+            ],
           },
           {
             title: 'Actions',
-            items: ['Test3', 'Test4', 'AND MORREEE', 'tehe'],
+            items: [
+              {
+                name: 'Asdf',
+                path: 'features',
+                index: 27,
+              },
+            ],
           },
           {
             title: 'Bonus Actions',
-            items: ['Test5', 'Test6'],
+            items: [
+              {
+                name: 'Asdf',
+                path: 'spells',
+                index: 8,
+              },
+            ],
           },
           {
             title: 'Reactions',
-            items: ['Test7', 'Test8'],
+            items: [
+              {
+                name: 'Asdf',
+                path: 'features',
+                index: 29,
+              },
+            ],
           },
         ],
-        action: null,
       };
     },
     computed: {
       character() {
         return find(this.$store.state.characters, { name: this.$route.params.name }) || {};
       },
+      actionData() {
+        return [
+          { title: 'Level', value: this.action.level },
+          { title: 'Casting Time', value: this.action.casting_time },
+          { title: 'Range', value: this.action.range },
+          { title: 'Components', value: this.action.components.join(', ') },
+          { title: 'Duration', value: this.action.duration },
+          { title: 'School', value: this.action.school.name },
+        ];
+      },
     },
     methods: {
-      getAbility() {
-        axios.get('http://www.dnd5eapi.co/api/spells/5')
+      getAbility({ path, index }) {
+        axios.get(`http://www.dnd5eapi.co/api/${path}/${index}`)
           .then(({ data }) => {
             console.log(data);
             this.action = data;
