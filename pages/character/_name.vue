e1f9503f7fc62b87a56f6aeaf4c97cc5f20e8c2e
DaleMatthews <dem7w2@mst.edu>
2018-02-03 22:00:44 -0600

    format v-list like a nav and call dnd5eapi

--- pages/character/_name.vue
+++ pages/character/_name.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-container fluid grid-list-xl>
-    <v-layout col fill-height justify-left align-center>
-      <v-flex xs3>
+  <v-container fill-height fluid pa-0>
+    <v-layout col justify-left align-center>
+      <v-flex d-flex xs3 pl-0 fill-height >
         <v-list>
           <v-list-group v-for="item in categories" :value="item.active" :key="item.title">
             <v-list-tile slot="item" @click="">
@@ -15,7 +15,7 @@
                 <v-icon>keyboard_arrow_down</v-icon>
               </v-list-tile-action>
             </v-list-tile>
-            <v-list-tile v-for="subItem in item.items" v-bind:key="subItem" @click="">
+            <v-list-tile v-for="subItem in item.items" v-bind:key="subItem" @click="getAbility">
               <v-list-tile-content>
                 <v-list-tile-title>{{ subItem }}</v-list-tile-title>
               </v-list-tile-content>
@@ -28,13 +28,14 @@
         </v-list>
       </v-flex>
       <v-flex xs9>
-        <div>{{ character.name }}</div>
+        {{ action && action.name }}
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+  import axios from 'axios';
   import { find } from 'lodash';
 
   export default {
@@ -58,11 +59,21 @@
             items: ['Test7', 'Test8'],
           },
         ],
+        action: null,
       };
     },
     computed: {
       character() {
         return find(this.$store.state.characters, { name: this.$route.params.name }) || {};
+      },
+    },
+    methods: {
+      getAbility() {
+        axios.get('http://www.dnd5eapi.co/api/spells/5')
+          .then(({ data }) => {
+            console.log(data);
+            this.action = data;
+          });
       },
     },
   };
