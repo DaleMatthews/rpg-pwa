62ed54f9ddc0115816ff44032d3c8b7a15c1b5d7
DaleMatthews <dem7w2@mst.edu>
2018-02-04 23:12:12 -0600

    Add ability to search and add spells, and make it persistent

--- pages/character/_name.vue
+++ pages/character/_name.vue
@@ -1,12 +1,21 @@
 <template>
   <v-container fill-height fluid pa-0>
 
-    <v-dialog v-model="showAddActionForm" max-width="500px">
+    <v-dialog persistent v-model="showAddActionForm" max-width="500px">
       <v-card>
         <v-card-title class="headline">New Action</v-card-title>
         <v-card-text>
-
+          <v-select
+              :items="allSpells" item-value="url" item-text="name"
+              v-model="newSpell"
+              label="Find Spell"
+              autocomplete
+          ></v-select>
         </v-card-text>
+        <v-card-actions>
+          <v-btn :loading="gettingAction" @click="addNewAction(newSpell)">Save</v-btn>
+          <v-btn @click="showAddActionForm = false; newSpell = ''">Cancel</v-btn>
+        </v-card-actions>
       </v-card>
     </v-dialog>
 
@@ -14,6 +23,7 @@
       <v-flex d-flex xs3 pl-0 fill-height>
 
         <v-list>
+          <!-- TODO auto focus search input -->
           <v-list-tile class="light-blue" @click="showAddActionForm = true" v-ripple>
             <v-list-tile-action>
               <v-icon medium>add</v-icon>
@@ -22,7 +32,7 @@
           </v-list-tile>
 
           <v-list-group v-for="item in categories" :value="item.active" :key="item.title">
-            <v-list-tile slot="item" @click="">
+            <v-list-tile slot="item" @click="" :disabled="!item.items.length">
               <v-list-tile-action>
                 <v-icon medium>{{ item.icon }}</v-icon>
               </v-list-tile-action>
@@ -33,7 +43,7 @@
                 <v-icon>keyboard_arrow_down</v-icon>
               </v-list-tile-action>
             </v-list-tile>
-            <v-list-tile v-for="subItem in item.items" :key="subItem.name" @click="getAbility(subItem)" v-ripple>
+            <v-list-tile v-for="subItem in item.items" :key="subItem.name" @click="getAction(subItem)" v-ripple>
               <v-list-tile-content>
                 <v-list-tile-title>{{ subItem.name }}</v-list-tile-title>
               </v-list-tile-content>
@@ -46,6 +56,7 @@
           <div class="display-1">{{ action.name }}</div>
           <v-layout py-4 wrap align-content-start>
 
+            <!-- TODO separate into component and handle non-spells -->
             <v-flex pa-2 xs4 md3 v-for="a in actionData" :key="a.title">
               <div class="title">{{ a.title }}</div>
               <div class="body-1">{{ a.value }}</div>
@@ -69,59 +80,38 @@
   import { find } from 'lodash';
 
   export default {
+    async asyncData() {
+      // TODO get non-spells too
+      const { data } = await axios.get('http://www.dnd5eapi.co/api/spells');
+
+      return { allSpells: data.results || [] };
+    },
     data() {
       return {
         action: null,
         showAddActionForm: false,
-        categories: [
-          {
-            title: 'Standard Actions',
-            icon: 'fa-book',
-            items: [
-              {
-                name: 'Asdf',
-                path: 'spells',
-                index: 1,
-              },
-            ],
-          },
-          {
-            title: 'Actions',
-            icon: 'fa-crosshairs',
-            items: [
-              {
-                name: 'Asdf',
-                path: 'spells',
-                index: 27,
-              },
-            ],
-          },
-          {
-            title: 'Bonus Actions',
-            icon: 'exposure_plus_1',
-            items: [
-              {
-                name: 'Asdf',
-                path: 'spells',
-                index: 8,
-              },
-            ],
-          },
-          {
-            title: 'Reactions',
-            icon: 'refresh',
-            items: [
-              {
-                name: 'Asdf',
-                path: 'spells',
-                index: 29,
-              },
-            ],
-          },
-        ],
+        newSpell: '',
+        gettingAction: false,
       };
     },
     computed: {
+      categories() {
+        const allActions = this.character.actions;
+        if (!allActions) return [];
+
+        const actions = allActions.filter(a => a.type === 'Action');
+        const bonusActions = allActions.filter(a => a.type === 'Bonus Action');
+        const reactions = allActions.filter(a => a.type === 'Reaction');
+        const others = allActions.filter(a => a.type === 'Other');
+
+        return [
+          { title: 'Standard Actions', icon: 'fa-book', items: [] }, // TODO create these
+          { title: 'Actions', icon: 'fa-crosshairs', items: actions },
+          { title: 'Bonus Actions', icon: 'exposure_plus_1', items: bonusActions },
+          { title: 'Reactions', icon: 'refresh', items: reactions },
+          { title: 'Others', icon: 'timer', items: others },
+        ];
+      },
       character() {
         return find(this.$store.state.characters, { name: this.$route.params.name }) || {};
       },
@@ -137,15 +127,22 @@
       },
     },
     methods: {
-      getAbility({ path, index }) {
-        axios.get(`http://www.dnd5eapi.co/api/${path}/${index}`)
+      getAction({ url, path, index }) {
+        // TODO set category.active
+        axios.get(url || `http://www.dnd5eapi.co/api/${path}/${index}`)
           .then(({ data }) => {
-            console.log(data);
             this.action = data;
           });
       },
-      addNewAction() {
-        console.log('Action added');
+      addNewAction(actionUrl) {
+        this.gettingAction = true;
+        this.$store.dispatch('addActionToCharacter', { actionUrl, character: this.character })
+          .then(() => {
+            this.gettingAction = false;
+            this.newSpell = '';
+            this.showAddActionForm = false;
+            this.getAction({ url: actionUrl });
+          });
       },
     },
   };
--- store/index.js
+++ store/index.js
@@ -1,27 +1,65 @@
+import axios from 'axios';
 import localforage from 'localforage';
+
+const getActionType = (action) => { // TODO separate into resource file
+  switch (action.casting_time) {
+    case '1 action':
+      return 'Action';
+    case '1 bonus action':
+      return 'Bonus Action';
+    case '1 reaction':
+      return 'Reaction';
+    default:
+      return 'Other';
+  }
+};
 
 export const state = () => ({
   characters: [],
 });
 
 export const mutations = {
-  addCharacters(state, characters) {
-    state.characters = state.characters.concat(characters);
+  addCharacter(state, character) {
+    state.characters.push(character);
   },
   setCharacters(state, characters) {
     state.characters = [...characters];
   },
+  addActionToCharacter(state, { character, action }) {
+    const { name } = character;
+    const { path, index, type } = action;
+    let i = state.characters.findIndex(c => c.name === name);
+    let char = { ...state.characters[i] };
+
+    char.actions = char.actions.filter(s => s.path !== path || s.index !== index).concat([{ name: action.name, path, index, type }]);
+
+    state.characters.splice(i, 1, char);
+  },
 };
 
 export const actions = {
-  addCharacter(context, { name, img }) {
-    const newVal = context.state.characters.concat([{ name, img }]);
-    localforage.setItem('characters', newVal).then(values => context.commit('addCharacters', [{ name, img }]));
+  addCharacter({ state, commit }, { name, img }) {
+    const newChar = { name, img, actions: [] };
+    commit('addCharacter', newChar);
+
+    return localforage.setItem('characters', JSON.parse(JSON.stringify(state.characters)));
   },
   getCharacters({ commit }) {
     return localforage.getItem('characters').then(values => {
       if (values) commit('setCharacters', values);
       return values || [];
     });
+  },
+  addActionToCharacter({ commit, state }, { actionUrl, character }) {
+    return axios.get(actionUrl)
+      .then(({ data }) => {
+        const action = { name: data.name, path: 'spells', index: data.index, type: getActionType(data) };
+
+        commit('addActionToCharacter', { character, action });
+
+        return localforage.setItem('characters', JSON.parse(JSON.stringify(state.characters)));
+      }).catch((err) => {
+        console.log(err); // TODO add error notification
+      });
   },
 };
