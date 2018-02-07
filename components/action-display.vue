0a6af02f4429839d152093a5aa875e1a8a7d22a0
DaleMatthews <dem7w2@mst.edu>
2018-02-06 22:20:03 -0600

    Make action-display a functional component

--- components/action-display.vue
+++ components/action-display.vue
@@ -1,42 +1,40 @@
-<template>
-  <div v-if="action && action.desc">
-    <div class="display-1">{{ action.name }}</div>
+<template functional>
+  <div v-if="props.action && props.action.desc">
+    <div class="display-1">{{ props.action.name }}</div>
     <v-layout py-4 wrap align-content-start>
 
-      <v-flex pa-2 xs4 md3 v-for="a in actionData" :key="a.title">
-        <div class="title">{{ a.title }}</div>
-        <div class="body-1">{{ a.value }}</div>
+      <!-- spell properties -->
+      <v-flex pa-2 xs4 md3>
+        <div class="title">Level</div>
+        <div class="body-1">{{ props.action.level }}</div>
+      </v-flex>
+      <v-flex pa-2 xs4 md3>
+        <div class="title">Casting Time</div>
+        <div class="body-1">{{ props.action.casting_time }}</div>
+      </v-flex>
+      <v-flex pa-2 xs4 md3>
+        <div class="title">Range</div>
+        <div class="body-1">{{ props.action.range }}</div>
+      </v-flex>
+      <v-flex pa-2 xs4 md3>
+        <div class="title">Components</div>
+        <div class="body-1">{{ props.action.components.join(', ') }}</div>
+      </v-flex>
+      <v-flex pa-2 xs4 md3>
+        <div class="title">Duration</div>
+        <div class="body-1">{{ props.action.duration }}</div>
+      </v-flex>
+      <v-flex pa-2 xs4 md3>
+        <div class="title">School</div>
+        <div class="body-1">{{ props.action.school.name }}</div>
       </v-flex>
 
-      <v-flex pt-3 class="subheading" v-for="d in action.desc">{{ d }}</v-flex>
-
-      <v-flex pt-3 class="subheading" v-if="action.higher_level">
-        <strong>At higher levels: </strong>{{ action.higher_level.join(' ') }}
+      <!-- description and higher level effects -->
+      <v-flex pt-3 class="subheading" v-for="d in props.action.desc">{{ d }}</v-flex>
+      <v-flex pt-3 class="subheading" v-if="props.action.higher_level">
+        <strong>At higher levels: </strong>{{ props.action.higher_level.join(' ') }}
       </v-flex>
 
     </v-layout>
   </div>
 </template>
-
-<script>
-  export default {
-    props: {
-      action: {
-        type: Object,
-        required: true,
-      },
-    },
-    computed: {
-      actionData() {
-        return [
-          { title: 'Level', value: this.action.level },
-          { title: 'Casting Time', value: this.action.casting_time },
-          { title: 'Range', value: this.action.range },
-          { title: 'Components', value: this.action.components.join(', ') },
-          { title: 'Duration', value: this.action.duration },
-          { title: 'School', value: this.action.school.name },
-        ];
-      },
-    },
-  };
-</script>
