b77ddb49a2be3303d044270d422ed107726cf971
DaleMatthews <dem7w2@mst.edu>
2018-02-18 21:31:30 -0600

    Put about inside a v-card and add now alias

--- pages/about.vue
+++ pages/about.vue
@@ -2,7 +2,11 @@
   <v-container fill-height>
     <v-layout row wrap align-center justify-center>
       <v-flex xs12 md6 text-xs-center>
-        <p class="headline">This is a side project of mine to facilitate my exploration of technologies such as vue 2, nuxt, vuetify, service workers, lighthouse, zeit now, firebase, and localforage. My secondary goal is to showcase a combat-centric UI that many D&D player apps often lack.</p>
+        <v-card>
+          <v-card-text>
+            <p class="headline">This is a side project of mine to facilitate my exploration of technologies such as vue 2, nuxt, vuetify, service workers, lighthouse, zeit now, firebase, and localforage. My secondary goal is to showcase a combat-centric UI that many D&D player apps often lack.</p>
+          </v-card-text>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
